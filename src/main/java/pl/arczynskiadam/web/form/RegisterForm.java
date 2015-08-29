797863c9a430b01e0942d691c9197d6452521c9a
Karister <adam.arczynski@gmail.com>
2015-08-29 20:50:29 +0200

    register form validation fix

--- src/main/java/pl/arczynskiadam/web/form/RegisterForm.java
+++ src/main/java/pl/arczynskiadam/web/form/RegisterForm.java
@@ -15,19 +15,16 @@ import pl.arczynskiadam.web.form.validation.NickAvailable;
 })
 public class RegisterForm {
 	
-	@NotBlank(message = "{register.nick.length.incorrect}")
 	@Size(min = 3, max = 20, message = "{register.nick.length.incorrect}")
 	@NickAvailable(message = "{register.nick.unavailable}")
 	private String nick;
 	
-	@NotBlank(message = "{register.password.length.incorrect}")
 	@Size(min = 8, message = "{register.password.length.incorrect}")
 	private String password;
 	
 	@NotBlank(message = "{validation.common.required}")
 	private String passwordConfirm;
 	
-	@NotBlank(message = "{validation.common.email.incorrect}")
 	@Pattern(regexp = "^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\\.[a-zA-Z]{2,4}$", message = "{validation.common.email.incorrect}")
 	@EmailAvailable(message = "{register.email.unavailable}")
 	private String email;
